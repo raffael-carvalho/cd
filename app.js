00849c25ec03f301f6e83b6cfa30976597ee6065
raffael-carvalho <ffaelcdec@gmail.com>
2025-11-09 12:55:59 -0300

    Commit inicial do projeto Olá Mundo DevOps

---
+++ app.js
@@ -0,0 +1,11 @@
+// Separamos a lógica do app da inicialização do servidor
+// para que os testes possam carregar o app sem iniciar o servidor.
+//
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).send("Olá Mundo DevOps, Proteger a Branch main e CD na AWS!");
+});
+
+module.exports = app; // Exportamos o app
---
+++ app.test.js
@@ -0,0 +1,17 @@
+const request = require("supertest");
+const app = require("./app"); // Importamos nosso app
+
+describe("API Olá Mundo", () => {
+  it('Deve retornar "Olá Mundo DevOps, Proteger a Branch main e CD na AWS!" na rota /', async () => {
+    // Faz uma requisição GET para a rota /
+    const response = await request(app).get("/");
+
+    // Verifica se o status code é 200 (OK)
+    expect(response.statusCode).toBe(200);
+
+    // Verifica se o corpo da resposta é o esperado
+    expect(response.text).toBe(
+      "Olá Mundo DevOps, Proteger a Branch main e CD na AWS!"
+    );
+  });
+});
